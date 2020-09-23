fc28389fd0d833ff40420c14da8a5f9e4d7db117
emanningt <emanningmusic@yahoo.com>
2020-09-23 16:34:46 -0700

    Update index.js
    Adding my end points points for when I build out my funtions for my fetch request.
    I also addend en event listender for the dome content loading.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Hello World');
+
+const categoryEndPoint = 'http://localhost:3000/api/v1/categories';
+const noteEndPoint = 'http://localhost:3000/api/v1/notes';
+
+document.addEventListener('DOMContentLoaded', () => {
+	console.log('Hello Level Two');
+});

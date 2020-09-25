fb41174e3cd950afde91ea92929575c26269a534
emanningt <emanningmusic@yahoo.com>
2020-09-25 13:50:01 -0700

    Update index.js
    I added a function to fetch my category data while console loging it to test if its working.

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const noteEndPoint = 'http://localhost:3000/api/v1/notes';
 
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('Hello Level Two');
+	getCategory;
 });
+
+function getCategory() {
+	fetch(categoryEndPoint).then((res) => res.json()).then((cat) => {
+		console.log(cat);
+	});
+}

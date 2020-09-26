326f9cbb7293ce28faf93fb625b4e2b29f070bac
emanningt <emanningmusic@yahoo.com>
2020-09-26 07:54:27 -0700

    Update index.js
    Added a getNotes function to fetch for my notes. I am consol loging to ensure that I have everything set up properly.

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const noteEndPoint = 'http://localhost:3000/api/v1/notes';
 
 document.addEventListener('DOMContentLoaded', () => {
 	console.log('Hello Level Two');
-	getCategory;
+	getCategories();
+	getNotes();
 });
 
-function getCategory() {
+function getCategories() {
 	fetch(categoryEndPoint).then((res) => res.json()).then((cat) => {
 		console.log(cat);
+	});
+}
+
+function getNotes() {
+	fetch(noteEndPoint).then((res) => res.json()).then((note) => {
+		console.log(note);
 	});
 }

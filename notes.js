2f3370c2fbcd14cbd53717d192c521ff02cc855f
emanningt <emanningmusic@yahoo.com>
2020-09-27 08:20:42 -0700

    Update notes.js
    Added to my notes class for OOJS. I added the notes data id and some atributes I think ill use.

--- notes.js
+++ notes.js
@@ -1,3 +1,22 @@
-class Note {}
+class Note {
+
+    constructor(noteData, noteAttributes) {
+        console.log("Note layer")
+        //debugger
+        this.id = noteData.id
+        this.title = noteAttributes.title
+        this.content = noteAttributes.content;
+        this.category_id = noteAttributes.category_id;
+        Note.all.push(this)
+    }
+
+    renderNotes() {
+        return `
+        <div data-id=${this.id}>
+        <h3>Title: ${this.title} </h3>
+        <h3>Content: ${this.content} </h3>
+        </div>`
+    }
+}
 
 Note.all = [];

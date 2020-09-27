ac5a7a6e3a763706975f17f3837613a5ff3a5182
emanningt <emanningmusic@yahoo.com>
2020-09-27 08:24:38 -0700

    Update categories.js
    I added more to my renderCategory method. This is where i would want each note to be returned to the proper id of the category. I will move on my index.js file next to see if i can get them synced properly.

--- categories.js
+++ categories.js
@@ -1,8 +1,10 @@
 class Category {
 	constructor(categoryData, categoryAttributes) {
-		console.log(categoryData);
+		//debugger
+		console.log("layer 4 ");
 		this.id = categoryData.id;
 		this.name = categoryAttributes.name;
+
 
 		Category.all.push(this);
 	}
@@ -10,9 +12,16 @@ class Category {
 	renderCategory() {
 		return `
     <div data-id=${this.id}>
-        <h3>${this.title} </h3>
-        <h3>${this.name} </h3>
-        </div>`;
+		<h3>${this.name} </h3>
+		</div>
+		<div id="note-container-${this.id}">
+    
+			</div>
+			<form id="note-form" data-id="${this.id}">
+            <input id="note-name" data-id=${this.id} type="text" name="name" value="" placeholder="Note Title" class="input-text">
+            <button class="input" data-id=${this.id}>Add Note</button>
+            </form>`;
+
 	}
 }
 

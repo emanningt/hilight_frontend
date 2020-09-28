cd3488fe0658dc0212ba49dbbf54368ee5665f01
emanningt <emanningmusic@yahoo.com>
2020-09-28 14:53:59 -0700

    Update categories.js
    I cleaned up my code on tis page to make it more readable for myself.

--- categories.js
+++ categories.js
@@ -4,7 +4,6 @@ class Category {
 		console.log("layer 4 ");
 		this.id = categoryData.id;
 		this.name = categoryAttributes.name;
-
 		//debugger
 
 		Category.all.push(this);
@@ -13,15 +12,19 @@ class Category {
 	renderCategory() {
 		return `
 		
-    <div id=${this.id}>
+    	<div id=${this.id}>
 		<h3>${this.name} </h3>
+		</div>
+		<div id="note-content-${this.id}">
 		
 		</div>
-		<div id="note-content-${this.id}"> </div>
-		<form id="note-form" data-id="${this.id}">
+		<form id="create-note" data-id="${this.id}">
 		<input id="note-name" data-id=${this.id} type="text" name="name" value="" placeholder="Note Title" class="input-text">
+		<br> </br>
 		<button class="input" data-id=${this.id}>Add Note</button>
-		</form>`;
+		</form>
+		
+		`;
 
 	}
 }

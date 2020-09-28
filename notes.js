289ea38f9fdda4d283c3005b4788586006f77b61
emanningt <emanningmusic@yahoo.com>
2020-09-28 14:57:58 -0700

    Update notes.js
    I re structured my notes file for something that works a little better with returning my notes for the database.

--- notes.js
+++ notes.js
@@ -1,18 +1,18 @@
 class Note {
 
-    constructor(noteData, noteAttributes) {
-        console.log("Note layer")
+    constructor(note) {
         //debugger
-        this.id = noteData.id
-        this.title = noteAttributes.title
-        this.content = noteAttributes.content;
-        this.category_id = noteAttributes.category_id;
+        this.id = note.id;
+        this.title = note.attributes.title;
+        this.content = note.attributes.content;
+        this.category_id = note.attributes.category_id;
+
         Note.all.push(this)
     }
 
     renderNotes() {
         return `
-        <div data-id=${this.id}>
+        <div id=${this.id}>
         <h3>Title: ${this.title} </h3>
         <h3>Content: ${this.content} </h3>
         </div>`;

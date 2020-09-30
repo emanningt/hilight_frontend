cffe8332e892bff8a4766e504b713017fa06ffb2
emanningt <emanningmusic@yahoo.com>
2020-09-30 12:33:50 -0700

    Update categories.js
    I added the data id for category so i know what to reference when the input is selected.

--- categories.js
+++ categories.js
@@ -17,12 +17,12 @@ class Category {
 		</div>
 		<div id="note-content-${this.id}">
 		
-		</div>
-		<form id="create-note" data-id="${this.id}">
+		<form id="create-note-${this.id}" data-id="${this.id}">
 		<input id="note-name" data-id=${this.id} type="text" name="name" value="" placeholder="Note Title" class="input-text">
 		<br> </br>
-		<button class="input" data-id=${this.id}>Add Note</button>
+		<button class="input-" data-id=${this.id}>Add Note</button>
 		</form>
+		</div>
 		
 		`;
 
